5c932a303f6859bdab16b610d0a77f4699502066
Artem Puchenkin <apuchenkin@gmail.com>
2024-12-01 07:50:30 +0000

    Day1

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,18 +1,24 @@
 module Day1 (first, second) where
 
-import Debug.Trace (traceShowId)
+import Data.List (sort)
 
-parse :: String -> [String]
-parse text = res
+parse :: String -> ([Int], [Int])
+parse text = (fst <$> pairs, snd <$> pairs)
   where
     res = lines text
+    pairs = parseLine <$> res
+    parseLine line = (read left, read right)
+      where
+        [left, right] = words line
 
 first :: String -> String
-first input = show $ length $ traceShowId n
+first input = show $ sum distances
   where
-    n = parse input
+    (left, right) = parse input
+    distances = abs <$> zipWith (-) (sort right) (sort left)
 
 second :: String -> String
-second input = show $ length $ traceShowId n
+second input = show $ sum $ process <$> left
   where
-    n = parse input
+    (left, right) = parse input
+    process x = x * length (filter (== x) right)
