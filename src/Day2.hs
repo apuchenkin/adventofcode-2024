4d9c0897dedf1a6f6768a2785bc240cab0f4cbd9
Artem Puchenkin <apuchenkin@gmail.com>
2024-12-02 09:50:52 +0000

    Day 2

--- src/Day2.hs
+++ src/Day2.hs
@@ -1,18 +1,28 @@
 module Day2 (first, second) where
 
-import Debug.Trace (traceShowId)
+import Control.Monad (liftM2)
 
-parse :: String -> [String]
-parse text = res
+parse :: String -> [[Int]]
+parse text = fmap read . words <$> res
   where
     res = lines text
 
-first :: String -> String
-first input = show $ length $ traceShowId n
+isSafe :: [Int] -> Bool
+isSafe line = (isPositive diffs || isNegative diffs) && inRange diffs
   where
-    n = parse input
+    diffs = zipWith (-) (init line) (tail line)
+    isPositive = all (> 0)
+    isNegative = all (< 0)
+    inRange = all (liftM2 (&&) (>= 1) (<= 3) . abs)
+
+first :: String -> String
+first input = show $ length $ filter isSafe ranges
+  where
+    ranges = parse input
 
 second :: String -> String
-second input = show $ length $ traceShowId n
+second input = show $ length $ filter (\l -> isSafe l || any isSafe (sublists l)) ranges
   where
-    n = parse input
+    ranges = parse input
+    dropAt k l = take k l ++ drop (k + 1) l
+    sublists list = flip dropAt list <$> [0 .. length list]
