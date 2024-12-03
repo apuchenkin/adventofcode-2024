3476a47f409f68205f8d2393daa31620c160e10a
Artem Puchenkin <apuchenkin@gmail.com>
2024-12-03 09:02:59 +0000

    Day 3

--- src/Day3.hs
+++ src/Day3.hs
@@ -1,18 +1,57 @@
 module Day3 (first, second) where
 
-import Debug.Trace (traceShowId)
+import Data.Either (fromRight)
+import qualified Text.Parsec as P
+import qualified Text.ParserCombinators.Parsec as PC
 
-parse :: String -> [String]
-parse text = res
+expressionParser :: PC.Parser (Int, Int)
+expressionParser = do
+  _ <- P.string "mul"
+  _ <- P.char '('
+  left <- P.many1 P.digit
+  _ <- P.char ','
+  right <- P.many1 P.digit
+  _ <- P.char ')'
+  return (read left, read right)
+
+doParser :: PC.Parser Bool
+doParser = P.string "do()" >> return True
+
+dontParser :: PC.Parser Bool
+dontParser = P.string "don't()" >> return False
+
+expressions :: PC.Parser [(Int, Int)]
+expressions = loop
   where
-    res = lines text
+    loop =
+      (P.eof >> return [])
+        P.<|> do r <- P.try expressionParser; rs <- loop; return (r : rs)
+        P.<|> (P.anyChar >> loop)
+
+extractEnabled :: PC.Parser String
+extractEnabled = loop True
+  where
+    loop state =
+      (P.eof >> return "")
+        P.<|> do r <- P.try doParser P.<|> dontParser; loop r
+        P.<|> do c <- P.anyChar; rs <- loop state; return $ if state then c : rs else rs
+
+parse :: String -> [(Int, Int)]
+parse text = fromRight (error "no-op") res
+  where
+    res = P.parse expressions "expressionParser" text
+
+parse2 :: String -> [(Int, Int)]
+parse2 text = fromRight (error "no-op") $ P.parse expressions "expressionParser" res
+  where
+    res = fromRight (error "no-op") $ P.parse extractEnabled "doParser" text
 
 first :: String -> String
-first input = show $ length $ traceShowId n
+first input = show $ sum multiples
   where
-    n = parse input
+    multiples = uncurry (*) <$> parse input
 
 second :: String -> String
-second input = show $ length $ traceShowId n
+second input = show $ sum multiples
   where
-    n = parse input
+    multiples = uncurry (*) <$> parse2 input
