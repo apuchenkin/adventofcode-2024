12b2ea2ff8150a1d456e3ed6ae3e4757a33c9688
Artem Puchenkin <apuchenkin@gmail.com>
2024-12-05 09:12:53 +0000

    Day 5

--- src/Day5.hs
+++ src/Day5.hs
@@ -1,18 +1,51 @@
 module Day5 (first, second) where
 
-import Debug.Trace (traceShowId)
+import Data.List (elemIndex, sortBy)
+import Data.List.Split (splitOn)
+import Data.Maybe (isJust, isNothing)
 
-parse :: String -> [String]
-parse text = res
+type Rule = (Int, Int)
+
+type Page = [Int]
+
+type Input = ([Rule], [Page])
+
+parse :: String -> Input
+parse text = (parseRule <$> lines rules, fmap read . splitOn "," <$> lines pages)
   where
-    res = lines text
+    [rules, pages] = splitOn "\n\n" text
+    parseRule line = (read left, read right)
+      where
+        [left, right] = splitOn "|" line
+
+matchRule :: Page -> Rule -> Bool
+matchRule page (left, right)
+  | isNothing leftIdx = True
+  | isNothing rightIdx = True
+  | otherwise = leftIdx <= rightIdx
+  where
+    leftIdx = elemIndex left page
+    rightIdx = elemIndex right page
+
+middle :: [a] -> a
+middle list = list !! (length list `div` 2)
 
 first :: String -> String
-first input = show $ length $ traceShowId n
+first input = show $ sum $ middle <$> matches
   where
-    n = parse input
+    (rules, pages) = parse input
+    matches = filter (\p -> all (matchRule p) rules) pages
+
+order :: [Rule] -> Page -> Page
+order rules = sortBy predicate
+  where
+    predicate left right
+      | isJust $ elemIndex (left, right) rules = LT
+      | isJust $ elemIndex (right, left) rules = GT
+      | otherwise = EQ
 
 second :: String -> String
-second input = show $ length $ traceShowId n
+second input = show $ sum $ middle . order rules <$> matches
   where
-    n = parse input
+    (rules, pages) = parse input
+    matches = filter (\page -> not $ all (matchRule page) rules) pages
