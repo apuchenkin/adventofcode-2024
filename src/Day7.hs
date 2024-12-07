b60340fbeee40d6d5a5309a6b3c43361e779e6aa
Artem Puchenkin <apuchenkin@gmail.com>
2024-12-07 20:23:58 +0000

    Day 7

--- src/Day7.hs
+++ src/Day7.hs
@@ -1,18 +1,28 @@
 module Day7 (first, second) where
 
-import Debug.Trace (traceShowId)
+import Data.List.Split (splitOn)
 
-parse :: String -> [String]
-parse text = res
+parse :: String -> [(Int, [Int])]
+parse text = parseLine <$> lines text
   where
-    res = lines text
+    parseLine line = (read left, read <$> splitOn " " right)
+      where
+        [left, right] = splitOn ": " line
+
+isValid :: (Int, [Int]) -> Bool
+isValid (_, []) = error "no-op"
+isValid (result, x : xs) = result `elem` matches
+  where
+    matches = foldl (\acc v -> [(+) v, (*) v] <*> acc) [x] xs
+
+isValid2 :: (Int, [Int]) -> Bool
+isValid2 (_, []) = error "no-op"
+isValid2 (result, x : xs) = result `elem` matches
+  where
+    matches = foldl (\acc v -> [(+) v, (*) v, \x -> read $ show x ++ show v] <*> acc) [x] xs
 
 first :: String -> String
-first input = show $ length $ traceShowId n
-  where
-    n = parse input
+first input = show $ sum $ fst <$> filter isValid (parse input)
 
 second :: String -> String
-second input = show $ length $ traceShowId n
-  where
-    n = parse input
+second input = show $ sum $ fst <$> filter isValid2 (parse input)
