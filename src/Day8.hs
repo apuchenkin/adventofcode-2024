62b02390e7153d8025316f46fc9ef9d772f61897
Artem Puchenkin <apuchenkin@gmail.com>
2024-12-08 12:55:38 +0000

    Day 8

--- src/Day8.hs
+++ src/Day8.hs
@@ -1,18 +1,57 @@
 module Day8 (first, second) where
 
-import Debug.Trace (traceShowId)
+import Data.Bifunctor (Bifunctor (bimap))
+import Data.List (nub)
+import qualified Data.Map as M
+import qualified Data.Matrix as MX
+import Data.Maybe (isJust)
 
-parse :: String -> [String]
-parse text = res
+parse :: String -> MX.Matrix Char
+parse text = MX.fromLists $ lines text
+
+antinodes :: (Int, Int) -> (Int, Int) -> [(Int, Int)]
+antinodes (x, y) (x', y') = [left, right]
   where
-    res = lines text
+    left = (x - (x' - x), y - (y' - y))
+    right = (x' + (x' - x), y' + (y' - y))
+
+complements :: [(Int, Int)] -> [(Int, Int)]
+complements xs = coordinates
+  where
+    pairs = (,) <$> xs <*> xs
+    coordinates = concatMap (filter (`notElem` xs) . uncurry antinodes) pairs
+
+antinodes2 :: MX.Matrix a -> (Int, Int) -> (Int, Int) -> [(Int, Int)]
+antinodes2 mtx (x, y) (x', y')
+  | x' == x && y' == y = [(x, y), (x', y')]
+  | otherwise = takeWhile (inside mtx) lefts ++ takeWhile (inside mtx) rights
+  where
+    xs = (*) (x' - x) <$> [1 ..]
+    ys = (*) (y' - y) <$> [1 ..]
+    lefts = bimap (x -) (y -) <$> zip xs ys
+    rights = bimap (x' +) (y' +) <$> zip xs ys
+
+complements2 :: MX.Matrix a -> [(Int, Int)] -> [(Int, Int)]
+complements2 mtx xs = nub $ filter (inside mtx) coordinates
+  where
+    pairs = (,) <$> xs <*> xs
+    coordinates = concatMap (uncurry (antinodes2 mtx)) pairs
+
+inside :: MX.Matrix a -> (Int, Int) -> Bool
+inside mtx (x, y) = isJust (MX.safeGet x y mtx)
 
 first :: String -> String
-first input = show $ length $ traceShowId n
+first input = show $ length $ filter (inside mtx) antinodes
   where
-    n = parse input
+    mtx = parse input
+    coordinates = filter ((/= '.') . snd) $ MX.toList $ MX.mapPos (,) mtx
+    cmap = M.fromListWith (++) $ (\(a, b) -> (b, [a])) <$> coordinates
+    antinodes = nub $ concatMap snd $ M.toList $ complements <$> cmap
 
 second :: String -> String
-second input = show $ length $ traceShowId n
+second input = show $ length antinodes
   where
-    n = parse input
+    mtx = parse input
+    coordinates = filter ((/= '.') . snd) $ MX.toList $ MX.mapPos (,) mtx
+    cmap = M.fromListWith (++) $ (\(a, b) -> (b, [a])) <$> coordinates
+    antinodes = nub $ concatMap snd $ M.toList $ complements2 mtx <$> cmap
