672b6795c90ccba6a95d291c6f1bff58dabbdbc5
Artem Puchenkin <apuchenkin@gmail.com>
2024-12-25 09:17:15 +0000

    Day 25

--- src/Day25.hs
+++ src/Day25.hs
@@ -1,18 +1,27 @@
 module Day25 (first, second) where
 
-import Debug.Trace (traceShowId)
+import Data.List (group, partition)
+import Data.List.Split (splitOn)
+import qualified Data.Matrix as MX
 
-parse :: String -> [String]
-parse text = res
+parse :: String -> [MX.Matrix Char]
+parse text = MX.fromLists . lines <$> splitOn "\n\n" text
+
+convert :: MX.Matrix Char -> [Int]
+convert mtx = head . fmap (pred . length) . group <$> cols
   where
-    res = lines text
+    cols = MX.toLists $ MX.transpose mtx
+
+isOverlap :: MX.Matrix Char -> MX.Matrix Char -> Bool
+isOverlap left right = not . or $ MX.toList $ MX.elementwise (\a b -> a == '#' && b == '#') left right
 
 first :: String -> String
-first input = show $ length $ traceShowId n
+first input = show $ length $ filter id $ isOverlap <$> locks <*> keys
   where
-    n = parse input
+    grids = parse input
+    (locks, keys) = partition (all (== '#') . MX.getRow 1) grids
 
 second :: String -> String
-second input = show $ length $ traceShowId n
+second input = show $ convert <$> n
   where
     n = parse input
